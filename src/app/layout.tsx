3481ebdf1bb43831c758c49f0999ae113a1d753c
Dennis Kimathi <kimathidennis38@gmail.com>
2025-06-26 14:23:06 +0300

    test: test deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-
 export const metadata: Metadata = {
   title: "Spicey O",
-  description: "Welcome to Spicey-O, Kenya's boldest small-batch condiment makers. Born out of the desire to make bland food better, we handcraft chutneys and sauces using 100% locally sourced Kenyan ingredients. Whether you prefer it mellow or 'very freakin’ hot', our range is designed to bring bold, unforgettable flavour to your plate. We’re proudly Kenyan—rooted in local culture, fuelled by passion, and obsessed with great food.",
+  description:
+    "Welcome to Spicey-O, Kenya's boldest small-batch condiment makers. Born out of the desire to make bland food better, we handcraft chutneys and sauces using 100% locally sourced Kenyan ingredients. Whether you prefer it mellow or 'very freakin’ hot', our range is designed to bring bold, unforgettable flavour to your plate. We’re proudly Kenyan—rooted in local culture, fuelled by passion, and obsessed with great food.",
 };
 
 export default function RootLayout({
@@ -13,12 +13,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en"   suppressHydrationWarning>
-      <body
-      
-      >
-        {children}
-      </body>
+    <html lang="en" suppressHydrationWarning>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,32 @@
+"use client";
+import React from "react";
+import {
+  AppCtaSection,
+  FeatureSection,
+  Header,
+  HeroSection,
+  ContactUsSection,
+  ProductCarousel,
+  FaqsSection,
+  WheatSection,
+  Footer,
+} from "@/components/landing-page-sections";
 
-
-"use client"
-import React from 'react';
-import { AppCtaSection, FeatureSection, Header, HeroSection, ContactUsSection, ProductCarousel, FaqsSection, WheatSection, Footer } from '@/components/landing-page-sections';
-
-
-// Main App Component
-export default function App() {
+// Main Home Page Component
+export default function HomePage() {
   return (
     <div className="bg-[#F8F5F0] antialiased">
-
-  
-        <main className="m-6">
+      <main className="m-6">
         <Header />
-
-            <HeroSection />
-            <FeatureSection />
-            <WheatSection />
-            <ProductCarousel />
-            <AppCtaSection />
-            <FaqsSection />
-            <ContactUsSection />
-        </main>
-        <Footer />
-      </div>
-    
+        <HeroSection />
+        <FeatureSection />
+        <WheatSection />
+        <ProductCarousel />
+        <AppCtaSection />
+        <FaqsSection />
+        <ContactUsSection />
+      </main>
+      <Footer />
+    </div>
   );
 }
